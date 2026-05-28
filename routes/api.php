e03f9c5de6c289f7545a507dc608e942137baa30
cybermanbg-cpu <cybermanbg@gmail.com>
2026-05-28 13:15:49 +0300

    fix doc

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,9 @@ use App\Http\Controllers\Api\DonationStatsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/donations-stats', [DonationStatsController::class, 'index']);
+
+// Основен маршрут
+Route::post('/owntracks', [App\Http\Controllers\Api\OwnTracksController::class, 'publish']);
+
+// За всеки случай, ако OwnTracks добавя /pub
+Route::post('/owntracks/pub', [App\Http\Controllers\Api\OwnTracksController::class, 'publish']);
