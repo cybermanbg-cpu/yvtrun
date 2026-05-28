fb1ac601bc4a4602e43272f8bbf7924131a57d34
cybermanbg-cpu <cybermanbg@gmail.com>
2026-05-28 13:25:31 +0300

    тест

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,12 @@ Route::post('/owntracks', [App\Http\Controllers\Api\OwnTracksController::class, 
 
 // За всеки случай, ако OwnTracks добавя /pub
 Route::post('/owntracks/pub', [App\Http\Controllers\Api\OwnTracksController::class, 'publish']);
+
+Route::post('/owntracks/test', function (Request $request) {
+    \Log::info('TEST OwnTracks received', [
+        'all_headers' => $request->headers->all(),
+        'payload' => $request->all()
+    ]);
+    
+    return response()->json([]);
+});
